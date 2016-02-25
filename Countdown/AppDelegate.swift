c176e240e6a7eac24436763981b976c0d98bff6f
Anne Cahalan <Anne@Annes-MacBook-Pro.local>
2016-02-25 09:04:26 -0500

    Initial commit
    Sets up status bar icon
    hides doc icon and window

--- Countdown/AppDelegate.swift
+++ Countdown/AppDelegate.swift
@@ -12,16 +12,25 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "statusBarIcon")
+            button.action = Selector("printLyric:")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func printLyric(sender: AnyObject) {
+        let lyric = "Look around, look around, how lucky we are to be alive right now"
+        print(lyric)
+    }
 
 }
 
