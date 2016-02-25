3e13d4ea03722d20e2152de94fef0d9b24d82333
Anne Cahalan <Anne@annes-macbook-pro.local>
2016-02-25 08:32:29 -0500

    Initial Commit

---
+++ Countdown/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Countdown
+//
+//  Created by Anne Cahalan on 2/25/16.
+//  Copyright © 2016 Anne Cahalan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
